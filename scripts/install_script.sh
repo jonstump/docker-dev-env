c2228a602575639d43f3460b16d3bbdf2140be12
Jon Stump <jmstump@gmail.com>
2021-11-10 14:01:48 -0800

    will change name of install script

---
+++ scripts/install_script.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#install ruby-installer
+wget -O ruby-install-0.8.2.tar.gz https://github.com/postmodern/ruby-install/archive/v0.8.2.tar.gz
+tar -xzvf ruby-install-0.8.2.tar.gz
+cd ruby-install-0.8.2/
+sudo make install
+
+#install chruby
+
+#install nvm
+
+#install ranger
