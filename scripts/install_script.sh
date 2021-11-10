71ae6c1a717c2dd074089464973d2cd3b6b21dcf
Jon Stump <jmstump@gmail.com>
2021-11-10 14:12:52 -0800

    update install script to include chruby and nvm

--- scripts/install_script.sh
+++ scripts/install_script.sh
@@ -7,8 +7,13 @@ cd ruby-install-0.8.2/
 sudo make install
 
 #install chruby
+wget -O chruby-0.3.9.tar.gz https://github.com/postmodern/chruby/archive/v0.3.9.tar.gz
+tar -xzvf chruby-0.3.9.tar.gz
+cd chruby-0.3.9/
+sudo make install
 
 #install nvm
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
 #install ranger
 wget -O ranger-stable.tar.gz https://ranger.github.io/ranger-stable.tar.gz
